f56f30502958cd330456184d6bff3027df594033
rrruko <rukokarasu@gmail.com>
2023-05-08 15:22:09 -0700

    update iohk-nix to latest

--- flake.nix
+++ flake.nix
@@ -135,8 +135,8 @@
               plutus-contract.flags.defer-plugin-errors = deferPluginErrors;
               cardano-crypto-praos.components.library.pkgconfig =
                 nixpkgs.lib.mkForce [ [ pkgs.libsodium-vrf ] ];
-              cardano-crypto-class.components.library.pkgconfig =
-                nixpkgs.lib.mkForce [ [ pkgs.libsodium-vrf ] ];
+              /*cardano-crypto-class.components.library.pkgconfig =
+                nixpkgs.lib.mkForce [ [ pkgs.libsodium-vrf ] ];*/
               beam-migrate.flags.werror = false;
             } //
             (setGHCOptions packages);
